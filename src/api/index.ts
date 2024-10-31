b63a380bd84b973e39887e4c8c7e8214ee9cf0f2
nanda widiarta <nandawidiarta@gmail.com>
2024-10-31 19:38:39 +0700

    vercel json aja

--- src/api/index.ts
+++
@@ -1,7 +0,0 @@
-import { web } from "../application/web";
-
-const PORT = process.env.PORT || 3000;
-
-web.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
